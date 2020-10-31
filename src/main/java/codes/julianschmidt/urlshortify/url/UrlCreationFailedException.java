1ed3ebb452d2704a97b876eb4dd64c031e96cdd6
Julian Schmidt <julian.schmidt@nexusgroup.com>
2020-10-31 11:33:11 +0100

    delete unused exception classes

--- src/main/java/codes/julianschmidt/urlshortify/url/UrlCreationFailedException.java
+++
@@ -1,7 +0,0 @@
-package codes.julianschmidt.urlshortify.url;
-
-public class UrlCreationFailedException extends RuntimeException {
-    public UrlCreationFailedException(String url) {
-        super(String.format("Failed to create URL '%s'", url));
-    }
-}
--- src/main/java/codes/julianschmidt/urlshortify/url/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package codes.julianschmidt.urlshortify.url;
-
-public class UrlNotFoundException extends RuntimeException {
-    public UrlNotFoundException(String key) {
-        super(String.format("Could not find url '%s'", key));
-    }
-}
