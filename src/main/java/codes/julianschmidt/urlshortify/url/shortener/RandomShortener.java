0db00bd7a8316a27ade01e7cd6d5e22bd9972e8c
Julian Schmidt <julian.schmidt@nexusgroup.com>
2020-11-06 19:09:43 +0100

    add logging to RandomShortener

--- src/main/java/codes/julianschmidt/urlshortify/url/shortener/RandomShortener.java
+++ src/main/java/codes/julianschmidt/urlshortify/url/shortener/RandomShortener.java
@@ -1,13 +1,17 @@
 package codes.julianschmidt.urlshortify.url.shortener;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import codes.julianschmidt.urlshortify.url.UrlRepository;
 import codes.julianschmidt.urlshortify.url.model.Url;
 
 public class RandomShortener implements UrlShortener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RandomShortener.class);
     private static final int DIGITS = 6;
+
     private final UrlRepository repository;
 
     public RandomShortener(UrlRepository repository) {
@@ -17,6 +21,7 @@ public class RandomShortener implements UrlShortener {
     @Override
     public Url decorateShortenedUrl(Url original) {
         String shortUrl = getUniqueRandomString();
+        LOGGER.debug("unique random string was generated: {}", shortUrl);
         original.setShortUrl(shortUrl);
         return original;
     }
@@ -24,7 +29,9 @@ public class RandomShortener implements UrlShortener {
     private String getUniqueRandomString() {
         while (true) {
             String candidate = generateRandomString();
+            LOGGER.trace("random string candidate generated: {}", candidate);
             if (isUnique(candidate)) {
+                LOGGER.trace("{} is unique", candidate);
                 return candidate;
             }
         }
